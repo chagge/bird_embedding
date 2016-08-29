51736d352e95347b6c01af9090892326135e5c4c
lipingliulp <liping.liulp@gmail.com>
2016-08-29 10:12:33 -0400

    add some plot of stixel-bird embedding

--- explain/explain.py
+++ explain/explain.py
@@ -1,51 +1,51 @@
-#import itertools
 import glob
 import os
-#import sys
-#os.environ['OPENBLAS_NUM_THREADS'] = '1'
-#
 import numpy as np
-#import matplotlib
-#matplotlib.use('Agg')
-#import matplotlib.pyplot as plt
-#
 import pandas as pd
-#from scipy import sparse
-#import seaborn as sns
-#sns.set(context="paper", font_scale=1.5, rc={"lines.linewidth": 2}, font='DejaVu Serif')
-#
-#
-#sys.path.append('../../cofactor/src/')
-#import cofacto
-#import rec_eval 
 
 import csv
 from scipy.spatial import distance
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+from adjustText import adjust_text
+import itertools
 
-n_components = 20
-
-
-#train_data = pd.read_csv('../data/aggregated_obs_2005.csv')
-#head = train_data[train_data.INDEX == -1]
-#names = head.iloc[:, 1:]
-#names.to_csv('../data/bird_names.csv', index=False)
+n_components = 3
 
 with open('../data/bird_names.csv', 'rb') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',')
     bird_names = spamreader.next()
 
-save_dir = os.path.join('../data/', 'ML20M_ns%d_scale%1.2E' % (1, 0.03))
+save_dir = '../data/result/cofactor12-14/'
 
 n_params = len(glob.glob(os.path.join(save_dir, 'CoFacto_K' + str(n_components) + '_iter*.npz')))
 params = np.load(os.path.join(save_dir, 'CoFacto_K%d_iter%d.npz' % (n_components, n_params - 1)))
 V = params['V']
 
-plt.scatter(V[:, 0], V[:, 1])
+icx = 1
+icy = 2
+d = np.sqrt(V[:, icx] * V[:, icx] + V[:, icy] * V[:, icy])
+threshold = 0.02
+flag = d > threshold 
+
+selected = [s for (s, b) in zip(bird_names, flag) if b]  
+compx = V[flag, icx]
+compy = V[flag, icy]
+
+plt.scatter(compx, compy)
+#for label, x, y in zip(selected, compx, compy):
+#    plt.annotate(label, xy = (x, y), xytext = (0, 0), textcoords = 'offset points', ha = 'right', va = 'bottom')
+
+texts = []
+for xt, yt, s in zip(compx, compy, selected):
+    if xt < 0.01:
+        texts.append(plt.text(xt, yt, s, size=8, ha = 'right', va = 'bottom'))
+    else:
+        texts.append(plt.text(xt, yt, s, size=8, ha = 'left', va = 'bottom'))
+
 plt.show()
 
-#print [bird_names[i] for i in np.where(V[:, 0] < -0.1)[0]]
-print [bird_names[i] for i in np.where(V[:, 1] > 0.12)[0]]
+#print [bird_names[i] for i in np.where(V[:, 0] > 0.1)[0]]
+print [bird_names[i] for i in np.where(V[:, 1] < -0.08)[0]]
 
 
 
