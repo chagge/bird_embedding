ca82b7634eed78883ad22bcf0bdb08bda83f4f21
lipingliulp <liping.liulp@gmail.com>
2016-08-31 16:43:14 -0700

    subset_data.py is modified to take another set of data

--- prepare_data/subset_data.py
+++ prepare_data/subset_data.py
@@ -1,5 +1,5 @@
 '''
-This code take a subset of ebird data by year, day range, and species
+This code take a subset of ebird data by year, day range, and location 
 Created on Aug 26, 2016
 
 @author: liuli
@@ -14,29 +14,33 @@ import sys
 if __name__ == '__main__':
     
     print 'Reading in data ...'
-
+    
     # selection conditions for rows
     # day range =  180 ~ 209
     # area range = lat: 39.716 ~ 42, long: -80.5 ~ -74.3
     # individual checklist
-
+    
     # selection conditions for columns
     # shown in the variable "flag"
-
+    
     year = 2014 
-
+    
     data_path = '/nfs/stak/students/l/liuli/liping/ebird/ebird_2014/' + str(year) + '/checklists.csv' 
     print 'Data path is ' + data_path
-
+    
     df = pandas.read_csv(data_path, sep=',', header='infer', na_values=['X', '?'], na_filter=True, keep_default_na=True)
-
-
-    flag = ((108 <= df['DAY']) & (df['DAY'] < 210) 
+    
+    
+    flag = ((180 <= df['DAY']) & (df['DAY'] < 210) 
           & ((39 + 43.0/60) < df['LATITUDE']) & (df['LATITUDE'] < 42)
           & (-(80 + 31.0/60) < df['LONGITUDE']) & (df['LONGITUDE'] < -74.3))
     
-    df = df.loc[flag, ['LATITUDE','LONGITUDE','DAY','TIME', 'COUNT_TYPE', 'EFFORT_HRS', 'EFFORT_DISTANCE_KM', 
-                     'EFFORT_AREA_HA', 'NUMBER_OBSERVERS', 'PRIMARY_CHECKLIST_FLAG', 'Contopus_virens']]
-
-    df.to_csv('../data/obs_subset_y' + str(year) + '_d180-210_ewp' + '.csv')
-
+    # if only take out eastern wood pewee
+    #df = df.loc[flag, ['LATITUDE','LONGITUDE','DAY','TIME', 'COUNT_TYPE', 'EFFORT_HRS', 'EFFORT_DISTANCE_KM', 
+    #                 'EFFORT_AREA_HA', 'NUMBER_OBSERVERS', 'PRIMARY_CHECKLIST_FLAG', 'Contopus_virens']]
+    
+    #df.to_csv('../data/obs_subset_y' + str(year) + '_d180-210_ewp' + '.csv')
+    
+    # if to take all species 
+    df = df.loc[flag, :]
+    #df.to_csv('../data/subset_pa/obs_subset_y' + str(year) + '_d180-210' + '.csv')
