8d3cb174d120b7149a6a0c7d3365ab5d20abf27a
lipingliulp <liping.liulp@gmail.com>
2016-09-09 10:07:12 -0400

    prepare train.tsv and test.tsv for poisson embedding

--- baselines/prepare_pemb_data.py
+++ baselines/prepare_pemb_data.py
@@ -1,26 +1,34 @@
 import numpy as np
 import pandas as pd
 import random
+import sys
 
+sys.path.append('../prepare_data/')
+
+from extract_counts import load_sparse_coo
 
 INPUT_DATA = '../data/subset_pa_201407/'
 
 filename = INPUT_DATA + 'counts.npz'
-loader = np.load(filename)
+sp_mat = load_sparse_coo(filename)
 
-data = loader.data
-row = loader.row
-col = loader.col
-n = len(data)
+data = sp_mat.data
+data = np.round(data)
+data[data == 0] = 1
+sp_mat.data = data
 
-data_mat = np.concatenate((np.reshape(data, (n, 1)), np.reshape(data, (n, 1)), np.reshape(data, (n, 1))), axis=1) 
+permind = np.arange(0, sp_mat.shape[0])
+random.shuffle(permind)
+train_ind = permind[0:(len(permind) / 2)]
+test_ind = permind[(len(permind) / 2) : len(permind)]
 
+train_mat = sp_mat.tocsr()[train_ind].tocoo()
+test_mat = sp_mat.tocsr()[test_ind].tocoo()
 
-permind = random.shuffle(range(0, len(data)))
-train_ind = permind[xrange(0,  (len(data) / 2))]
-test_ind = permind[xrange((len(data) / 2), len(permind))]
+train_quad = np.c_[np.ones((len(train_mat.data), 1)), train_mat.row, train_mat.col, train_mat.data]
+test_quad = np.c_[np.ones((len(test_mat.data), 1)), test_mat.row, test_mat.col, test_mat.data]
 
-numpy.savetxt(INPUT_DATA + 'p_emb_train.tsv', data_mat[train_ind, :], delimiter='\t')
-numpy.savetxt(INPUT_DATA + 'p_emb_test.tsv', data_mat[test_ind, :], delimiter='\t')
+np.savetxt(INPUT_DATA + 'train.tsv', train_quad, delimiter='\t', fmt='%d')
+np.savetxt(INPUT_DATA + 'test.tsv', test_quad, delimiter='\t', fmt='%d')
 
 
