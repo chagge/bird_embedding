9e8e94e8b204371270f6c4875c87ae4b0a0b4bf6
lipingliulp <liping.liulp@gmail.com>
2016-09-17 15:19:46 -0400

    first version of learning embedding

--- infer/svi.py
+++ infer/svi.py
@@ -8,12 +8,13 @@ import sys
 sys.path.append('../prepare_data/')
 from extract_counts import load_sparse_coo
 
+
 def expit(x):
     y = np.ones(x.shape)
     y[x > -20] = 1 / (1 + np.exp(-x[x > -20]))
     return y
 
-def elbo(obs_cov, counts, K, param, it):
+def elbo(counts, context, obs_cov, K, param):
     
     ntuple = counts.shape[0]
     nspecies = counts.shape[1]
@@ -31,7 +32,7 @@ def elbo(obs_cov, counts, K, param, it):
     # alternatively speaking: element j_1 contributes the same amount to species j_2 and j_3 in their respective contexts. 
     # weight is included in the context. 0 means the element is not in the context of any other species 
     # there is a lot of game to play with R 
-    R = (counts > 0).astype(float)
+    R = context 
 
 
     # calculate lambda
@@ -41,28 +42,28 @@ def elbo(obs_cov, counts, K, param, it):
     H0 = np.dot(R, np.diag(np.sum(alpha * rho, axis=1)))
     H = np.dot(R, np.dot(alpha, rho.T)) - H0
     
-    #if np.sum(np.isnan(U)) > 0:
-    #    raise Exception('NaN value in U')
-
-    #if np.sum(np.isnan(Lamb)) > 0:
-    #    raise Exception('NaN value in Lambda')
-    #if np.sum(np.isinf(Lamb)) > 0:
-    #    raise Exception('Inf value in Lambda')
-   
+    if np.sum(np.isnan(U)) > 0:
+        raise Exception('NaN value in U')
 
     #calculate q, the parameter of the variational parameter
 
-    #if np.sum(np.isnan(Q)) > 0:
-    #    raise Exception('NaN value in Lambda')
 
     epsilon = 1e-3
     Lamb = H.copy()
     Lamb[H < 20] = np.log(1 + np.exp(H[H < 20])) 
     Lamb = Lamb + epsilon
 
+    if np.sum(np.isnan(Lamb)) > 0:
+        raise Exception('NaN value in Lambda')
+    if np.sum(np.isinf(Lamb)) > 0:
+        raise Exception('Inf value in Lambda')
+ 
     Q = np.ones((ntuple, nspecies)) 
     flag = counts == 0
     Q[flag] = (U[flag] * np.exp(-Lamb[flag])) / (1 - U[flag] + U[flag] * np.exp(-Lamb[flag]))
+
+    if np.sum(np.isnan(Q)) > 0:
+        raise Exception('NaN value in Lambda')
 
     Temp = Q * (counts / Lamb - 1)  * expit(H)
     Temp1 = np.dot(R.T, Temp)
@@ -80,14 +81,61 @@ def elbo(obs_cov, counts, K, param, it):
     grad = np.r_[grad_alpha, grad_rho, grad_beta]
 
     norm = 0.5 * np.sum(alpha * alpha) + 0.5 * np.sum(rho * rho) + 0.5 * np.sum(beta * beta)
-    llh = np.sum((1 - Q) * np.log(1 - U) + Q * (counts * np.log(Lamb) - Lamb + np.log(U))) 
-    entropy = - np.sum((1 - Q[Q != 1]) * np.log(1 - Q[Q != 1])) - np.sum(Q[Q != 0] * np.log(Q[Q != 0]))
+    flag0 = Q > 1e-10
+    flag1 = Q < 1 - 1e-10
+    llh = np.sum((1 - Q[flag1]) * np.log(1 - U[flag1])) + np.sum(Q[flag0] * (counts[flag0] * np.log(Lamb[flag0]) - Lamb[flag0] + np.log(U[flag0]))) 
+    entropy = - np.sum((1 - Q[flag1]) * np.log(1 - Q[flag1])) - np.sum(Q[flag0] * np.log(Q[flag0]))
     obj = norm - (llh + entropy) / ntuple 
+
     
     return dict(obj=obj, grad=grad)
 
 
-def test_gradient(obs_cov, counts):
+def learn_embedding(counts, context, obs_cov, K):
+
+    ntuple = counts.shape[0]
+    nspecies = counts.shape[1]
+    ncovar = obs_cov.shape[1]
+
+    param = np.random.rand(nspecies *(2 * K + ncovar)) * 1e-3
+    rindex = np.arange(ntuple)
+    np.random.shuffle(rindex)
+    
+    valind = rindex[0 : ntuple/5] 
+    trind = rindex[ntuple/5 : ]
+
+    valobj =  elbo(counts[valind, :], context[valind, :], obs_cov[valind, :], K, param)
+    for it in xrange(1, 1000):
+        
+        #print('iteration ' + str(it) + ':' + str(max(param)))
+        rind = np.random.choice(trind, size=50, replace=False)
+        res = elbo(counts[rind, :], context[rind, :], obs_cov[rind, :], K, param)
+
+        grad = res['grad']
+        obj = res['obj']
+        
+        param = param - grad / (it + 100)
+
+        if it % 50 == 0:
+            #res = elbo(obs_cov, counts, K, param, it)
+            valres =  elbo(counts[valind, :], context[valind, :], obs_cov[valind, :], K, param)
+            print 'objective is ' + str(res['obj']) + '; validation objective is ' + str(valres['obj'])
+            if valres['obj'] > valobj:
+                break
+
+            valobj = valres['obj']
+
+    alpha = param[0 : nspecies * K] 
+    rho = param[nspecies * K : nspecies * K * 2]
+    beta = param[nspecies * K * 2 : ]
+
+    model = dict(alpha=alpha, rho=rho, beta=beta)
+    return model
+
+ 
+
+
+def test_gradient(counts, context, obs_cov):
 
     nspecies = counts.shape[1]
     ncovar = obs_cov.shape[1]
@@ -96,17 +144,16 @@ def test_gradient(obs_cov, counts):
     # intialize a parameter
     param = np.random.rand(nspecies *(2 * K + ncovar)) * 1e-3
 
-    res1 = elbo(obs_cov, counts, K, param, 1)
+    res1 = elbo(counts, context, obs_cov, K, param)
 
     param2 = param.copy()
-
     dalpha = 1e-8 * np.random.rand(nspecies * K)
     drho =  1e-8  * np.random.rand(nspecies * K)
     dbeta = 1e-8 * np.random.rand(nspecies * ncovar)
     dparam = np.r_[dalpha, drho, dbeta] 
     param2 = param + dparam
 
-    res2 = elbo(obs_cov, counts, K, param2, 1)
+    res2 = elbo(counts, context, obs_cov, K, param2)
     diffv = res2['obj'] - res1['obj']
     diffp = np.dot(res1['grad'], dparam) 
     print 'value difference is '
@@ -141,34 +188,11 @@ if __name__ == "__main__":
     data_dir = '../data/subset_pa_201407/'
     obs_cov = np.load(data_dir + 'obs_covariates.npy')
     counts = load_sparse_coo(data_dir + 'counts.npz').toarray()
+    context = (counts > 0).astype(float)
+    K = 10
 
-    test_gradient(obs_cov, counts)
-    raise Exception('Stop here')
+    #test_gradient(counts, context, obs_cov)
+    #raise Exception('Stop here')
    
-    ntuple = counts.shape[0]
-    nspecies = counts.shape[1]
-    ncovar = obs_cov.shape[1]
-
-    param = np.random.rand(nspecies *(2 * nspecies - 2 + ncovar)) * 1e-4
-
-    for it in xrange(1, 100000):
-        
-        #print('iteration ' + str(it) + ':' + str(max(param)))
-        rind = np.random.randint(0, ntuple, size=1000) 
-        
-        if it == 2 and False:
-            print('---------------------------------------------------------') 
-            print(max(param))
-        res = elbo_softplus(obs_cov[rind, :], counts[rind, :], param, it)
-        grad = res['grad']
-        
-        if it == 2 and False:
-            print('---------------------------------------------------------') 
-
-        param = param - grad / (it + 1000)
-
-        if it % 3 == 0:
-            fullres = elbo_softplus(obs_cov[rind, :], counts[rind, :], param, it)
-            print 'objective is ' + str(fullres['obj'])
-
-    
+    learn_embedding(counts, context, obs_cov, K)
+   
