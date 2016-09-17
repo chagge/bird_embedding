95969b7d1e1fefc17016fe0bb43a95a17db76223
lipingliulp <liping.liulp@gmail.com>
2016-09-17 15:38:06 -0400

    add file run_experiment to run different algorithms for comparison

---
+++ experiment/run_experiment.py
@@ -0,0 +1,33 @@
+'''
+This code runs experiment and compare different algorithms 
+Created on Sep 17, 2016
+
+@author: liuli
+
+'''
+import numpy as np
+import sys
+
+sys.path.append('../prepare_data/')
+from extract_counts import load_sparse_coo
+
+sys.path.append('../infer/')
+from learn_emb import learn_embedding 
+
+if __name__ == "__main__":
+
+    np.random.seed(4)
+    data_dir = '../data/subset_pa_201407/'
+    obs_cov = np.load(data_dir + 'obs_covariates.npy')
+    counts = load_sparse_coo(data_dir + 'counts.npz').toarray()
+    context = (counts > 0).astype(float)
+    K = 10
+
+    #test_gradient(counts, context, obs_cov)
+    #raise Exception('Stop here')
+   
+    model = learn_embedding(counts, context, obs_cov, K)
+ 
+
+
+
