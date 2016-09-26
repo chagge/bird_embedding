22bad6b8a7aa50858eb3536d2e418d5980daba48
Liping Liu <liping.liulp@gmail.com>
2016-09-26 18:19:26 -0400

    some minor changes

--- baselines/prepare_pemb_data.py
+++ baselines/prepare_pemb_data.py
@@ -11,17 +11,18 @@ INPUT_DATA = '../data/subset_pa_201407/'
 filename = INPUT_DATA + 'counts.npz'
 sp_mat = load_sparse_coo(filename)
 
+# round float numbers, which is missing and filled with averages, to integers
 data = sp_mat.data
 data = np.round(data)
 data[data == 0] = 1
 sp_mat.data = data
 
+# separate the data into three sets
 permind = np.arange(0, sp_mat.shape[0])
 random.shuffle(permind)
-train_size = len(permind) / 3
-val_size = len(permind) / 3
+train_size = round(len(permind) * (0.9 * 0.67))
+val_size = round(len(permind) * (0.1 * 0.67))
 test_size = len(permind) - (train_size + val_size)  
-
 train_ind = permind[0 : train_size]
 val_ind = permind[train_size : train_size + val_size]
 test_ind = permind[train_size + val_size : ]
@@ -30,6 +31,7 @@ train_mat = sp_mat.tocsr()[train_ind].tocoo()
 val_mat = sp_mat.tocsr()[val_ind].tocoo()
 test_mat = sp_mat.tocsr()[test_ind].tocoo()
 
+# write data sets into files
 train_quad = np.c_[np.ones((len(train_mat.data), 1)), train_mat.row, train_mat.col, train_mat.data]
 val_quad = np.c_[np.ones((len(val_mat.data), 1)), val_mat.row, val_mat.col, val_mat.data]
 test_quad = np.c_[np.ones((len(test_mat.data), 1)), test_mat.row, test_mat.col, test_mat.data]
