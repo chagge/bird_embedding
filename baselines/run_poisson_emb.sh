ab07e84fc89cf79a626014246772143d5f1cf3ec
Liping Liu <liping.liulp@gmail.com>
2016-10-02 01:28:13 -0400

    a good parameter setting for pemb

--- baselines/run_poisson_emb.sh
+++ baselines/run_poisson_emb.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo "Submit the pemb job to the system: "
-nohup ../lib/poisson_efe/pemb -dir ../data/subset_pa_201407/ -K 10 -max-iterations 10000 -Nthreads 3 > job.log &
+../lib/poisson_efe/pemb -dir ../data/subset_pa_201407/data_folds/0/ -K 10 -max-iterations 2000 -Nthreads 3 -zeroFactor 0.1 -eta 0.01 -s2alpha 1000 -s2rho 1000 > job0.log 
+
