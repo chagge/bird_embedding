7bbead6f48bf15ba37923a1babde9da718be7b34
Liping Liu <liping.liulp@gmail.com>
2016-10-07 12:34:44 -0400

    plot bird embedding

--- summarize/plot_emb.py
+++ summarize/plot_emb.py
@@ -2,19 +2,17 @@
 
 import sys
 import numpy as np
+import cPickle as pickle 
 
 import csv
 from matplotlib import pyplot as plt
 sys.path.append('../lib/tsne')
 from tsne import tsne
+sys.path.append('../experiment')
+from experiment import config_to_filename
 from matplotlib.backends.backend_pdf import PdfPages
 
-def plot_bird(alpha):
-
-    # read in bird names
-    with open('../data/bird_names.csv', 'rb') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',')
-        bird_names = spamreader.next()
+def plot_bird(alpha, bird_names):
 
     # perform t-SNE embedding
     vis_data = tsne(alpha)
@@ -52,14 +50,26 @@ def plot_bird(alpha):
 
 if __name__ == "__main__":
 
+    data_dir = '../data/subset_pa_201407/'
+
     fold = 5 
 
     model_config = dict(K=10, sigma2a=100, sigma2r=100, sigma2b=100, link_func='exp', intercept_term=False, scale_context=True, downzero=True, use_obscov=True)
-    filename = 'result/' + config_to_filename(model_config, fold=5)
+    filename = '../experiment/result/' + config_to_filename(model_config, fold=5)
     pkl_file = open(filename, 'rb') 
+    output = pickle.load(pkl_file)
     model = output['model']
+    alpha = model['alpha']
 
-    print model['alpha']
+    fold_dir = data_dir + 'data_folds/' + str(rseed) + '/'
+    species_ind = np.loadtxt(fold_dir + 'nonzero_ind.csv')
+    # read in bird names
+    with open('../data/bird_names.csv', 'rb') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',')
+        bird_names = spamreader.next()
+    
+    bird_names = bird_names[species_ind]
 
+    plot_bird(alpha, bird_names)
 
 
