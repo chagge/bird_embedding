dc75fd468ca7d9ae7d282c83c544b8ee3721707e
Liping Liu <liping.liulp@gmail.com>
2016-10-07 22:20:57 -0400

    better version of code for preparation of data. add the denoising part. Distributions have long tails, but we have to cut the tail

--- prepare_data/extract_counts.py
+++ prepare_data/extract_counts.py
@@ -9,6 +9,7 @@ Created on Aug 26, 2016
 import pandas as pd
 import numpy as np
 import scipy.sparse as sparse
+import csv
 
 
 def save_sparse_coo(filename, smat):
@@ -23,26 +24,45 @@ def load_sparse_coo(filename):
 
 def denoise(x):
 
+    x_back = x.copy()
     nzflag = x > 0 
     y = x[nzflag]
+
+    np.set_printoptions(threshold=np.inf)
+
+    #print "y------------------"
+    #print y
 
     sind = np.argsort(y) 
     z = np.sort(y)
 
+    #print "z------------------"
+    
+    #print z
+
     counter = np.arange(1, len(z) + 1)
     mean = np.cumsum(z).astype(float) / counter
-    var = np.cumsum(z * z).astype(float) / counter - mean * mean
+    var = np.sqrt(np.cumsum(z * z).astype(float) / counter - mean * mean)
 
-    threshold = np.r_[np.inf, var[0 : -1]] * 3
-    replacement = np.r_[1, mean[0 : -1]]
+    #print "mean and var------------------"
+    #print mean
+    #print var
 
-    repflag = z > threshold 
-    z[repflag] = replacement[repflag] 
-    
+    for i in xrange((len(z) - 1), 0, -1):
+        if z[i] > var[i-1] * 10 + mean[i - 1]:
+            z[i] = mean[i - 1]
+        else:
+            break
+
     y[sind] = z
 
     x[nzflag] = y
 
+    if not (i < (len(z)  - 1)) :
+        if np.sum(x_back != x) > 0:
+            raise Exception('Not recovering original array')
+            
+    return 0
 
 if __name__ == '__main__':
     
@@ -50,7 +70,7 @@ if __name__ == '__main__':
 
     print 'Reading in data ...'
     
-    ebird_data = pd.read_csv(folder + 'obs_subset_y2014_d180-210.csv')
+    ebird_data = pd.read_csv(folder + 'obs_subset.csv')
     
     counts = ebird_data.loc[:, 'Zenaida_macroura':].as_matrix()
     
@@ -65,6 +85,27 @@ if __name__ == '__main__':
         
     print '\nFilling NaN values...'
     counts = np.apply_along_axis(fill_positive_mean, 0, counts)
+
+
+    print 'remove rare species'
+    abd_species = np.sum(counts > 0, axis=0) > 5 
+    counts = counts[:, abd_species]
+
+    with open('../data/bird_names.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        bird_names = reader.next()
+
+    assert(len(bird_names) == 934)
+    abd_names = [bird_names[i] for i in np.arange(0, len(abd_species))[abd_species]] 
+
+    with open(folder + 'abd_bird_names.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(abd_names)
+
+
+    print '\nRemoving outliers...'
+    counts = np.apply_along_axis(denoise, 0, counts)
+
 
     path = folder + 'counts.npz'
     print '\nSaving to file ' + path + '...'
