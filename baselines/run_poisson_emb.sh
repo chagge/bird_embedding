497c3e2d96112991a92f061d46c80ec6e8440712
Liping Liu <liping.liulp@gmail.com>
2016-10-19 11:14:38 -0400

    some scripts of running jobs. They get changed constantly

--- baselines/run_poisson_emb.sh
+++ baselines/run_poisson_emb.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 echo "Submit the pemb job to the system: "
-../lib/poisson_efe/pemb -dir ../data/subset_pa_201407/data_folds/0/ -K 10 -max-iterations 2000 -Nthreads 3 -zeroFactor 0.1 -eta 0.01 -s2alpha 1000 -s2rho 1000 > job0.log 
+
+data_dir=../data/subset_pa_201407
+fold=1
+
+../lib/poisson_efe/pemb -dir $data_dir/data_folds/$fold/ -outdir $data_dir/result/pemb -K 10 -max-iterations 100 -Nthreads 4 -eta 0.01 -zeroFactor 0 -s2alpha 1 -s2rho 1 -avgContext 1 -rfreq 10000 > job0.log 
+#../lib/poisson_efe/pemb -dir ../data/subset_pa_201407/data_folds/0/ -K 10 -max-iterations 2000 -Nthreads 3 -zeroFactor 0.1 -eta 0.01 -s2alpha 1 -s2rho 1 > job0.log 
 
--- experiment/beaker_job.sh
+++ experiment/beaker_job.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for i in `seq 0 9`;
+for i in `seq 0 4`;
 do
     echo "Submit job for fold $i"
-    nohup python -c "import experiment; experiment.fold_learn(K=10, sigma2ar=100, sigma2b=100, link_func='exp', intercept_term=False, scale_context=True, downzero=True, use_obscov=True, fold=$i)" > log/job$i.log &
+    nohup python -c "import experiment; experiment.fold_learn(K=10, sigma2ar=100, sigma2b=100, link_func='softplus', intercept_term=True, scale_context=False, normalize_context=False, downzero=False, use_obscov=False, zeroweight=1.0, fold=$i)" > log/jobexp$i.log &
 done
 
 
