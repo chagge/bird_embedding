f1f4f1ff138997cb060141d56955cb8670f3e73a
Liping Liu <liping.liulp@gmail.com>
2016-11-23 17:54:08 -0500

    calculate co-purchase score

--- summarize/pair_birds.py
+++ summarize/pair_birds.py
@@ -11,32 +11,28 @@ sys.path.append('../lib/tsne')
 from tsne import tsne
 sys.path.append('../experiment')
 from experiment import config_to_filename
-from matplotlib.backends.backend_pdf import PdfPages
+sys.path.append('../infer')
+from emb_model import softplus
+from scipy.stats import poisson
 
 def bird_name_dict():
     taxonomy = pandas.read_csv('../data/taxonomy.csv', header=0) 
     bird_dict = dict(zip(taxonomy['SCI_NAME'], taxonomy['PRIMARY_COM_NAME']))
     return bird_dict
 
-def bird_color_code(sci_names):
-    taxonomy = pandas.read_csv('../data/taxonomy.csv', header=0) 
-    order_dict = dict(zip(taxonomy['PRIMARY_COM_NAME'], taxonomy['ORDER_NAME']))
-    orders = [order_dict[name] for name in sci_names]
-
-    orderlist = list(set(orders))
-    colorlist = ['rgb(146, 73,  0)',   'rgb(219, 209, 0)',   'rgb(36,  255, 36)', 
-                 'rgb(255, 255, 109)', 'rgb(73,  0,   146)', 'rgb(0,   109, 219)', 
-                 'rgb(182, 109, 255)', 'rgb(109, 182, 255)', 'rgb(182, 219, 255)', 
-                 'rgb(255, 182, 119)', 'rgb(255, 109, 182)', 'rgb(0,   146, 146)', 
-                 'rgb(0,   73,  73)',  'rgb(10,  10,  10)',  'rgb(146, 0,   0)',
-                 'rgb(0,   91,  146)', 'rgb(73,  91,  109)', 'rgb(91, 128,   200)']
-
-    colors = [colorlist[orderlist.index(order)] for order in orders] 
-
-    return colors 
-
-
 if __name__ == "__main__":
+
+    # read in bird names
+    bird_dict = bird_name_dict()
+    fold_dir = data_dir + 'data_folds/' + str(fold) + '/'
+    species_ind = np.loadtxt(fold_dir + 'nonzero_ind.csv', dtype=int)
+    with open(data_dir + 'abd_bird_names.csv', 'r') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',')
+        sci_names = spamreader.next()
+    
+    bird_names = [bird_dict[sci_names[ind]] for ind in species_ind]
+
+
 
     data_dir = '../data/subset_pa_201407/'
     fold = 0 
@@ -54,41 +50,21 @@ if __name__ == "__main__":
     model = output['emb_model'].model_param
     alpha = model['alpha']
     rho = model['rho']
-    
-
-    
-
-    score = rho.dot(alpha.T)
-    diff = score - score.T
-
-    sind = np.argsort(diff.flatten())
-    
-    subscript = np.unravel_index(sind, diff.shape)
-
-    # read in bird names
-    bird_dict = bird_name_dict()
-    fold_dir = data_dir + 'data_folds/' + str(fold) + '/'
-    species_ind = np.loadtxt(fold_dir + 'nonzero_ind.csv', dtype=int)
-    with open(data_dir + 'abd_bird_names.csv', 'r') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',')
-        sci_names = spamreader.next()
-    
-    bird_names = [bird_dict[sci_names[ind]] for ind in species_ind]
+    b = model['b']
 
 
-    ihawk = bird_names.index("American_Robin")
-    chind = np.argsort(- score[ihawk, :])
+    subject = 'Bald_Eagle'
+    ibird = bird_names.index(subject)
 
-    for i in xrange(0, 20):
-        ibird = ihawk
-        jbird = chind[i] 
-        print "---------------------------------------------------"
-        print "bird %s like %s with score: %f" % (bird_names[ibird], bird_names[jbird], rho[ibird, :].dot(alpha[jbird, :]))
+    lamb = softplus(rho.dot(alpha[ibird, :]) + b)
+    nzprob = 1 - poisson.pmf(np.zeros(lamb.shape), lamb)
+    co_score = nzprob * log(nzprob / (1 - nzprob))
 
-        ibird = ibird + jbird
-        jbird = ibird - jbird 
-        ibird = ibird - jbird 
-        print "bird %s like %s with score: %f" % (bird_names[ibird], bird_names[jbird], rho[ibird, :].dot(alpha[jbird, :]))
+    ind = np.argsort(- co_score)
+    co_names = [bird_names[i] for i in ind] 
+
+    print(zip(co_names, co_score))
+
 
 
 
